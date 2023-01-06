802420e5a21a3fbe3d76c2159cef8332eef905ff
Philip Demand <philipdemand@yahoo.com>
2023-01-06 11:16:26 -0500

    commit tree 9591aaa761e60a715147a7d92eb3ee3be5353a17

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  let saturdayWishes = `This Saturday, I want to ${activity}!`
+  return saturdayWishes
+}
+
+const mondayWork = function (activity = "go to the office") {
+    let message = `This Monday, I will ${activity}.`
+    return message
+}
+
+function wrapAdjective(flare = "*") {  
+    return function (adjective = 'special') {
+        let anotherVariable = `You are ${flare}${adjective}${flare}!`
+        return anotherVariable
+      }
+    }
